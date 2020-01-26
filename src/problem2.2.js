b63db91fa72a4fbb9a3a25fcbc7af2d50617e75d
zm00622 <zarymanning@gmail.com>
2020-01-26 13:15:33 -0500

    Just testing git with this commit

--- src/problem2.2.js
+++ src/problem2.2.js
@@ -2,37 +2,65 @@ import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
 import './styling.css';
 
-class CodingChallenge extends Component {
-  state = {
-    firstName: '',
-    showName: false
-  }
+// Testing commit
 
-  displayNameHandler = (e) => {
-    let updatedName = e.target.value;
-    this.setState({ firstName: updatedName });
-    //console.log(updatedName);
-  }
+function CodingChallenge() {
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.setState({
-      showName: true
-    });
-  }
+  const [updatedName, setUpdatedName] = React.useState('');
 
-  render() {
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(updatedName);
+      // code for error
+    };
+
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <h1>Enter Your Name</h1>
-          <input id="inputBox" type="text" name="firstName" onChange={this.displayNameHandler} value={this.state.firstName} />
-          <button type="submit" onClick={this.handleSubmit}>Submit</button>
-          {this.state.showName && <p>Hello {this.state.firstName}, welcome to the page.</p>}
+          <input
+            id="inputBox"
+            type="text"
+            name="updatedName"
+            value={updatedName}
+            onChange={e => setUpdatedName(e.target.value)}
+          />
+          <button>Submit</button>
+          <p>Hello {updatedName}, welcome to the page.</p>
         </form>
       </div>
     );
-  }
 }
 
 export default CodingChallenge;
+
+
+
+// The code below is an attempt at trying to figure out how to not have automatic update underneath the text box, but rather
+// to update the welcome msg only after the button is clicked again
+
+
+// displayNameHandler = (e) => {
+//   let updatedName = e.target.value;
+//   this.setState({ firstName: updatedName });
+//   //console.log(updatedName);
+// }
+//
+// handleSubmit = (e) => {
+//   e.preventDefault();
+//   this.setState({
+//     showName: true
+//   });
+// }
+
+
+
+
+
+//   resetForm = () => {
+//    this.setState({
+//        ...this.state,
+//        firstname: '',
+//        lastname: ''
+//    })
+// }
